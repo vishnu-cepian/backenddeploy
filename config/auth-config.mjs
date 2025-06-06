a3a0a2da6884975e3bb53d41096d5d5433e293f2
vishnu <vishnurpillai2003@gmail.com>
2025-06-06 19:40:30 +0530

    Error in importing environment variables

--- config/auth-config.mjs
+++ config/auth-config.mjs
@@ -1,3 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+if(!process.env.ACCESS_TOKEN_SECRET || !process.env.REFRESH_TOKEN_SECRET || !process.env.OTP_TOKEN_SECRET || !process.env.ADMIN_ACCESS_TOKEN_SECRET || !process.env.ADMIN_REFRESH_TOKEN_SECRET){
+    console.log("Missing environment variables");
+}
+
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "default_access_token_secret";
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || "default_refresh_token_secret";
 export const OTP_TOKEN_SECRET = process.env.OTP_TOKEN_SECRET || "default_otp_token_secret"
