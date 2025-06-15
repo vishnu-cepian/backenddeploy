6eda34eddd266b1b861ec69c84eb8a17c5f5acc6
vishnu <vishnurpillai2003@gmail.com>
2025-06-15 13:16:38 +0530

    To track monthly leadership vendors

---
+++ entities/LeaderboardHistory.mjs
@@ -0,0 +1,54 @@
+import { EntitySchema } from "typeorm";
+
+export const LeaderboardHistory = new EntitySchema({
+    name: "LeaderboardHistory",
+    tableName: "leaderboard_history",
+    columns: {
+        id: {
+            type: "uuid",
+            primary: true,
+            generated: "uuid"
+        },
+        vendorId: {
+            type: "uuid",
+            nullable: false
+        },
+        serviceType: {
+            type: "varchar",
+            nullable: false
+        },
+        monthYear: {
+            type: "varchar",
+            nullable: false
+        },
+        currentMonthRating: {
+            type: "numeric",
+            precision: 3,
+            scale: 2,
+            nullable: false
+        },
+        currentMonthReviewCount: {
+            type: "int",
+            nullable: false
+        },
+        bayesianScore: {
+            type: "numeric",
+            precision: 3,
+            scale: 2,
+            nullable: false
+        },
+        rank: {
+            type: "int",
+            nullable: false
+        },
+    },
+    relations: {
+        vendor: {
+            type: "many-to-one",
+            target: "Vendors",
+            joinColumn: { name: "vendorId" },
+            onDelete: "CASCADE",
+            cascade: true
+        }
+    }
+})
