95a64ca26aa970a4f0f40b5e6f4fc027208700ef
vishnu <vishnurpillai2003@gmail.com>
2025-06-26 23:09:26 +0530

    new entity

---
+++ entities/PaymentFailures.mjs
@@ -0,0 +1,70 @@
+import { EntitySchema } from "typeorm";
+
+export const PaymentFailures = new EntitySchema({
+    name: "PaymentFailures",
+    tableName: "paymentFailures",
+    columns: {
+        id: {
+            primary: true,
+            type: "uuid",
+            generated: "uuid"
+        },
+        orderId: {
+            type: "uuid",
+            nullable: false,
+        },
+        quoteId: {
+            type: "uuid",
+            nullable: false,
+        },
+        customerId: {
+            type: "uuid",
+            nullable: false,
+        },
+        paymentId: {
+            type: "uuid",
+            nullable: false,
+        },
+        amount: {
+            type: "decimal",
+            precision: 10,
+            scale: 2,
+            nullable: false,
+        },
+        reason: {
+            type: "varchar",
+            nullable: false,
+        },
+        status: {
+            type: "varchar",
+            nullable: false,
+        },
+        timestamp: {
+            type: "timestamp",
+            nullable: false,
+        },
+    },
+    relations: {    
+        order: {
+            type: "many-to-one",
+            target: "Orders",
+            joinColumn: { name: "orderId" },
+            onDelete: "CASCADE",
+            cascade: true
+        },
+        quote: {
+            type: "many-to-one",
+            target: "OrderQuotes",
+            joinColumn: { name: "quoteId" },
+            onDelete: "CASCADE",
+            cascade: true
+        },
+        customer: {
+            type: "many-to-one",
+            target: "Customers",
+            joinColumn: { name: "customerId" },
+            onDelete: "CASCADE",
+            cascade: true
+        },
+    },
+});
