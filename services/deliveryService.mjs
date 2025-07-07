826222022ce4bf678f6595361ec5231df0151e2e
vishnu <vishnurpillai2003@gmail.com>
2025-07-07 13:08:34 +0530

    new file for deliveryService fns

---
+++ services/deliveryService.mjs
@@ -0,0 +1,8 @@
+import { logger } from "../utils/logger-utils.mjs";
+import { sendError } from "../utils/core-utils.mjs";
+import { AppDataSource } from "../config/data-source.mjs";
+
+export const sendDeliveryRequest = async (payload) => {
+    throw new Error("Not implemented");
+    // console.log(payload)
+}
