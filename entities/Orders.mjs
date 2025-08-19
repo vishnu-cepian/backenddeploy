7e9a489f0e125f1d9d8cf62e074f7d8522ec3722
vishnu <vishnurpillai2003@gmail.com>
2025-08-19 21:59:21 +0530

    index updates in orders table

--- entities/Orders.mjs
+++ entities/Orders.mjs
@@ -4,6 +4,13 @@ import { ORDER_STATUS } from "../types/enums/index.mjs";
 export const Orders = new EntitySchema({
     name: "Orders",
     tableName: "orders",
+    indices: [
+        { name: "IDX_ORDERS_CUSTOMER_ID", columns: ["customerId"] },
+        { name: "IDX_ORDERS_SELECTED_VENDOR_ID", columns: ["selectedVendorId"] },
+        { name: "IDX_ORDERS_FINAL_QUOTE_ID", columns: ["finalQuoteId"] },
+        { name: "IDX_ORDERS_STATUS", columns: ["orderStatus"] },
+        { name: "IDX_ORDERS_ORDER_STATUS_TIMESTAMP", columns: ["orderStatusTimestamp"] },
+    ],
     columns: {
         id: {
             primary: true,
@@ -48,58 +55,17 @@ export const Orders = new EntitySchema({
         },
         clothProvided: {
             type: "boolean",
+            nullable: false,
             default: false,
-        },
-        fullName: {
-            type: "varchar",
-            nullable: false
-        },
-        phoneNumber: {
-            type: "varchar",
-            nullable: false
-        },
-        addressLine1: {
-            type: "varchar",
-            nullable: false
-        },
-        addressLine2: {
-            type: "varchar",
-            nullable: true
-        },
-        district: {
-            type: "varchar",
-            nullable: false
-        },
-        state: {
-            type: "varchar",
-            nullable: false
-        },
-        street: {
-            type: "varchar",
-            nullable: false
-        },
-        city: {
-            type: "varchar",
-            nullable: false
-        },
-        pincode: {
-            type: "varchar",
-            nullable: false
-        },
-        landmark: {
-            type: "varchar",
-            nullable: true
-        },
-        addressType: {
-            type: "varchar",
-            nullable: true
         },
         isPaid: {
             type: "boolean",
+            nullable: false,
             default: false
         },
         isRefunded: {
             type: "boolean",
+            nullable: false,
             default: false
         },
         serviceType: {
@@ -108,6 +74,7 @@ export const Orders = new EntitySchema({
         },
         isRated: {
             type: "boolean",
+            nullable: false,
             default: false
         },
         orderStatus: {
@@ -120,6 +87,19 @@ export const Orders = new EntitySchema({
             type: "jsonb",
             nullable: true
         },
+
+        fullName: { type: "varchar", nullable: false },
+        phoneNumber: { type: "varchar", nullable: false },
+        addressLine1: { type: "varchar", nullable: false },
+        addressLine2: { type: "varchar", nullable: true },
+        district: { type: "varchar", nullable: false },
+        state: { type: "varchar", nullable: false },
+        street: { type: "varchar", nullable: false },
+        city: { type: "varchar", nullable: false },
+        pincode: { type: "varchar", nullable: false },
+        landmark: { type: "varchar", nullable: true },
+        addressType: { type: "varchar", nullable: true },
+       
         createdAt: {
             type: "timestamp",
             createDate: true
@@ -136,6 +116,48 @@ export const Orders = new EntitySchema({
             joinColumn: { name: "customerId" },
             onDelete: "CASCADE",
             cascade: true
+        },
+        selectedVendor: {
+            type: "many-to-one",
+            target: "Vendors",
+            joinColumn: { name: "selectedVendorId" },
+            onDelete: "SET NULL",
+            cascade: true
+        },
+        finalQuote: {
+            type: "many-to-one",
+            target: "OrderQuotes",
+            joinColumn: { name: "finalQuoteId" },
+            onDelete: "SET NULL",
+            cascade: true
+        },
+        payment: {
+            type: "many-to-one",
+            target: "Payments",
+            joinColumn: { name: "paymentId" },
+            onDelete: "SET NULL",
+            cascade: true
+        },
+
+        orderItems: {
+            type: "one-to-many",
+            target: "OrderItems",
+            inverseSide: "order",
+        },
+        orderVendors: {
+            type: "one-to-many",
+            target: "OrderVendors",
+            inverseSide: "order",
+        },
+        orderStatusTimeline: {
+            type: "one-to-many",
+            target: "OrderStatusTimeline",
+            inverseSide: "order",
+        },
+        rating : {
+            type: "one-to-one",
+            target: "Rating",
+            inverseSide: "order",
         },
     }
 });
