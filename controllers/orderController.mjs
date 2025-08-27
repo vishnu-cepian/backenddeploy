ac827674bac3c8819415b0a26a9a276a04f48e55
vishnu <vishnurpillai2003@gmail.com>
2025-08-27 18:47:56 +0530

    moved webhook code from order files

--- controllers/orderController.mjs
+++ controllers/orderController.mjs
@@ -20,68 +20,12 @@ export const createOrder = async (req, res, next) => {
     }
 };
 
-// export const getOrders = async (req, res, next) => {
-//     try {
-//         const data = { userId: req.user.id };
-//         const response = await orderService.getOrders(data);
-//         if (!response) {
-//             throw new Error(formatError("Orders not found", response));
-//         }
-//         res.status(200).json(formatResponse(MESSAGE.SUCCESS, true, response));
-//     } catch (error) {
-//         logger.error(error);
-//         next(error);
-//     }
-// };  
-
-// export const getOrderById = async (req, res, next) => {
-//     try {
-//         const data = { userId: req.user.id, orderId: req.params.orderId };
-//         const response = await orderService.getOrderById(data);
-//         if (!response) {
-//             throw new Error(formatError("Order not found", response));
-//         }
-//         res.status(200).json(formatResponse(MESSAGE.SUCCESS, true, response));
-//     } catch (error) {
-//         logger.error(error);
-//         next(error);
-//     }
-// };
-
-export const deleteOrder = async (req, res, next) => {
-    try {
-        const data = { orderId: req.params.orderId };
-        const response = await orderService.deleteOrder(data);
-        if (!response) {
-            throw new Error(formatError("Order not deleted", response));
-        }
-        res.status(200).json(formatResponse(MESSAGE.SUCCESS, true, response));
-    } catch (error) {
-        logger.error(error);
-        next(error);
-    }
-};
-
 export const sendOrderToVendor = async (req, res, next) => {
     try {
         const data = {userId: req.user.id, ...req.body};
         const response = await orderService.sendOrderToVendor(data);
         if (!response) {
             throw new Error(formatError("Order not sent to vendor", response));
-        }
-        res.status(200).json(formatResponse(MESSAGE.SUCCESS, true, response));
-    } catch (error) {
-        logger.error(error);
-        next(error);
-    }
-};
-
-export const viewOrderVendorStatus = async (req, res, next) => {
-    try {
-        const data = { orderId: req.params.orderId };
-        const response = await orderService.viewOrderVendorStatus(data);
-        if (!response) {
-            throw new Error(formatError("Order vendor status not found", response));
         }
         res.status(200).json(formatResponse(MESSAGE.SUCCESS, true, response));
     } catch (error) {
@@ -116,76 +60,6 @@ export const createRazorpayOrder = async (req, res, next) => {
         logger.error(error);
         next(error);
     }   
-};
-
-export const viewAcceptedOrderDetails = async (req, res, next) => {
-    try {
-        const data = { orderId: req.params.orderId, vendorId: req.params.vendorId };
-        const response = await orderService.viewAcceptedOrderDetails(data);
-        if (!response) {
-            throw new Error(formatError("Order details not found", response));
-        }
-        res.status(200).json(formatResponse(MESSAGE.SUCCESS, true, response));
-    } catch (error) {
-        logger.error(error);
-        next(error);
-    }
-};
-
-export const viewReceivedOrderDetails = async (req, res, next) => {
-    try {
-        const data = { vendorId: req.params.vendorId };
-        const response = await orderService.viewReceivedOrderDetails(data);
-        if (!response) {
-            throw new Error(formatError("Order details not found", response));
-        }
-        res.status(200).json(formatResponse(MESSAGE.SUCCESS, true, response));
-    } catch (error) {
-        logger.error(error);
-        next(error);
-    }
-};
-
-export const initiateVendorPayment = async (req, res, next) => {
-    try {
-        const data = { orderId: req.params.orderId, vendorId: req.params.vendorId, customerId: req.params.customerId };
-        const response = await orderService.initiateVendorPayment(data);
-        if (!response) {
-            throw new Error(formatError("Payment not initiated", response));
-        }
-        res.status(200).json(formatResponse(MESSAGE.SUCCESS, true, response));
-    } catch (error) {
-        logger.error(error);
-        next(error);
-    }
-};
-
-export const confirmVendorPayment = async (req, res, next) => {
-    try {
-        const data = { paymentId: req.params.paymentId };
-        const response = await orderService.confirmVendorPayment(data);
-        if (!response) {
-            throw new Error(formatError("Payment not confirmed", response));
-        }
-        res.status(200).json(formatResponse(MESSAGE.SUCCESS, true, response));
-    } catch (error) {
-        logger.error(error);
-        next(error);
-    }
-};
-
-export const freezeOrderVendors = async (req, res, next) => {
-    try {
-        const data = { orderId: req.params.orderId, vendorId: req.params.vendorId };
-        const response = await orderService.freezeOrderVendors(data);
-        if (!response) {
-            throw new Error(formatError("Failed to freeze order vendors", response));
-        }
-        res.status(200).json(formatResponse(MESSAGE.SUCCESS, true, response));
-    } catch (error) {
-        logger.error(error);
-        next(error);
-    }
 };
 
 export const updateOrderStatus = async (req, res, next) => {
--- routes/orderRoutes.mjs
+++ routes/orderRoutes.mjs
@@ -2,39 +2,26 @@ import { Router } from "express";
 import * as orderController from "../controllers/orderController.mjs";
 import { verifyAccessToken } from "../middlewares/auth.mjs";
 import { controllerWrapper } from "../controllers/index.mjs";
-import { handleRazorpayWebhook } from "../services/orderService.mjs";
 
 const router = Router();
 
 // CUSTOMER ACCESSIBLE ROUTES
 
 router.post("/createOrder", verifyAccessToken, controllerWrapper(orderController.createOrder, { logRequest: true, logResponse: true }));
-// router.get("/getOrders", verifyAccessToken, controllerWrapper(orderController.getOrders, { logRequest: true, logResponse: true }));
-// router.get("/getOrderById/:orderId", verifyAccessToken, controllerWrapper(orderController.getOrderById, { logRequest: true, logResponse: true }));
-// router.delete("/deleteOrder/:orderId", verifyAccessToken, controllerWrapper(orderController.deleteOrder, { logRequest: true, logResponse: true }));
 
 router.post("/sendOrderToVendor", verifyAccessToken, controllerWrapper(orderController.sendOrderToVendor, { logRequest: true, logResponse: true }));
 
-// router.get("/viewOrderVendorStatus/:orderId", verifyAccessToken, controllerWrapper(orderController.viewOrderVendorStatus, { logRequest: true, logResponse: true }));
-// router.get("/viewAcceptedOrderDetails/:orderId/:vendorId", verifyAccessToken, controllerWrapper(orderController.viewAcceptedOrderDetails, { logRequest: true, logResponse: true }));
 
 // VENDOR ACCESSIBLE ROUTES
 
-// router.get("/viewReceivedOrderDetails/:vendorId", verifyAccessToken, controllerWrapper(orderController.viewReceivedOrderDetails, { logRequest: true, logResponse: true }));
 router.post("/vendorOrderResponse", verifyAccessToken, controllerWrapper(orderController.vendorOrderResponse, { logRequest: true, logResponse: true }));
-
-// PAYMENT ROUTES
 
 router.post("/createRazorpayOrder", verifyAccessToken, controllerWrapper(orderController.createRazorpayOrder, { logRequest: true, logResponse: true }));
 
-router.post("/handleRazorpayWebhook", handleRazorpayWebhook);
-
 router.post("/updateOrderStatus", verifyAccessToken, controllerWrapper(orderController.updateOrderStatus, { logRequest: true, logResponse: true }));
 
+// CUSTOMER & VENDOR ACCESSIBLE ROUTES
+
 router.get("/getOrderTimeline/:orderId", verifyAccessToken, controllerWrapper(orderController.getOrderTimeline, { logRequest: true, logResponse: true }));
-
-// router.post("/confirmVendorPayment/:paymentId", verifyAccessToken, controllerWrapper(orderController.confirmVendorPayment, { logRequest: true, logResponse: true }));
-
-// router.post("/freezeOrderVendors/:orderId/:vendorId", verifyAccessToken, controllerWrapper(orderController.freezeOrderVendors, { logRequest: true, logResponse: true }));
 
 export default router;
