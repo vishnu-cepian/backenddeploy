6faf8ce76f73c51c92371f4b2a95e432a65f517f
vishnu <vishnurpillai2003@gmail.com>
2025-09-12 19:40:29 +0530

    removed getMontlyLeadershipBoard()

--- routes/ratingRoutes.mjs
+++ routes/ratingRoutes.mjs
@@ -7,6 +7,5 @@ const router = Router();
 
 router.post("/updateVendorRating", verifyAccessToken, controllerWrapper(ratingController.updateVendorRating, { logRequest: true, logResponse: true })); // Update vendor rating
 router.get("/getDailyLeadershipBoard", verifyAccessToken, controllerWrapper(ratingController.getDailyLeadershipBoard, { logRequest: true, logResponse: true })); // Get daily leadership board
-router.post("/getMonthlyLeadershipBoard", verifyAccessToken, controllerWrapper(ratingController.getMonthlyLeadershipBoard, { logRequest: true, logResponse: true }));
 
 export default router;
